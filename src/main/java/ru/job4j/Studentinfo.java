46c851c10a148e438ff72e18afa1b10cc89bb222
kamikataty <kamikataty9140@gmail.com>
2024-01-09 11:41:06 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Tryapitsyn Sergei");
+        System.out.println("16.10.1991");
+    }
+}
