4f2fc4530564c0003602733d9188f821997d077f
kamikataty <kamikataty9140@gmail.com>
2024-06-11 09:45:01 +0300

    4.2. Результат работы метода [#232622]

---
+++ src/test/java/ru/job4j/Variable.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Variable {
+    public static void main(String[] args) {
+        byte age = 30;
+        System.out.println(age);
+        char sex = 'M';
+        System.out.println(sex);
+        float height = 1.7F;
+        System.out.println(height + " " + "м");
+        float cirpich = 100;
+        System.out.println(cirpich + " " + "м2");
+        double bacterii = 1.2345E+21;
+        System.out.println(bacterii + " " + "*" + " " + "10⁷" + " " + "КОЕ/г");
+        double distance = 1.2345E+21;
+        System.out.println(distance + " " + "км");
+        boolean workPermit = true;
+        System.out.println(workPermit);
+        long sizeFile = 28740;
+        System.out.println(sizeFile + " " + "byte");
+    }
+}
